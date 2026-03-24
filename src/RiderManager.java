0cc39d6ea21e6549453b7072cea2dd6a7ae9e6c6
Kanika <kanikakur14@gmail.com>
2026-03-24 15:16:19 +0530

    Added ridermanager and strategy manager to manage rider and select startegy

--- src/RiderManager.java
+++ src/RiderManager.java
@@ -1,2 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RiderManager {
+    private static RiderManager instance;
+    private final Map<String,Rider> riders=new HashMap<>();
+
+    public RiderManager(){}
+
+    public RiderManager getInstance(){
+        if(instance ==null){
+            return new RiderManager();
+        }
+        else {
+            return instance;
+        }
+    }
+
+    public void addRider(Rider rider){
+        riders.put(rider.getId(),rider);
+    }
+    public Rider getRider(String id){
+        return riders.get(id);
+    }
+
 }
--- src/StrategyManager.java
+++ src/StrategyManager.java
@@ -1,2 +1,19 @@
+import java.util.Objects;
+
 public class StrategyManager {
+    private static StrategyManager instance;
+
+    public StrategyManager(){}
+
+    public StrategyManager getInstance(){
+        return Objects.requireNonNullElseGet(instance, StrategyManager::new);
+    }
+
+    public PricingStrategy getPricingStrategy(TripMetaData data){
+        return new DefaultPricingStrategy();
+    }
+
+    public DriverMatchingStrategy getDriverMatchingStrategy(TripMetaData data){
+        return new NearestDriverStrategy();
+    }
 }
